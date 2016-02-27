7ac854602b2382fa44656bd63baa2889a8cca984
Wendy Chan <wendychanhku@gmail.com>
2016-02-27 13:44:15 +0000

    Reinitialize

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,7 @@
+require 'twitter'
+
+class TweetsController < ApplicationController
+
+twitter.update("@wendychanhk your @MyAvivaHome is shaking! Tweet photos to show damage @AvivaUKSupport")
+
+end
