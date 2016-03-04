d9567555b321f3cad44fbc918d008c52a8aaa08b
pavoni <mail@gregdowling.com>
2016-03-04 14:19:01 +0000

    Try another approach.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,7 @@
-if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
-end
+# if ENV["REDISCLOUD_URL"]
+#     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+# end
+
+uri = URI.parse(ENV["REDISCLOUD_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
