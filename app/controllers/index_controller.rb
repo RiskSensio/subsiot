0f0cd3c05a815e02ecdc7c7cb5634e8629ffdc7b
pavoni <mail@gregdowling.com>
2016-03-04 14:33:41 +0000

    Tidy.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,17 +2,9 @@ require 'twitter'
 
 class IndexController < ApplicationController
   def index
+    # Start sensor similator
     SimulateSensor.start
-
-    redirect_to page_path('candle')
-
-   #  client = Twitter::REST::Client.new do |config|
-   #   config.consumer_key = 'BLEAp7Hh1tvoR4lX20AGIrBn9'
-   #   config.consumer_secret = 	'zB8dKrfTFmBkmTSpv1V74LkoUdSK6UrR2adoGjYP3YgTvi3eKb'
-   #   config.access_token = '703567729215848448-QuCIS5Mg07O063wNRdRh3FSILNZv8qr'
-   #   config.access_token_secret = 'rLwx2s8gp3qa16EJ4r45vQc7xfk1Roc8675aY6ljgpRGe'
-   #  end
-
-   # @list = client.home_timeline
+    # Redirect ro demo page
+    redirect_to page_path('demo')
   end
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,5 @@
-# if ENV["REDISCLOUD_URL"]
-#     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
-# end
-
-uri = URI.parse(ENV["REDISCLOUD_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
+if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    Resque.redis = REDIS
+end
